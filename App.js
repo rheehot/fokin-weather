678c1fa1081a6462e1d7a76a7de5f5e03196e802
peter L <peter.l@uvaper.com>
2019-10-09 12:05:26 +0900

    no message

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
-  // return (
-  //   <View style={styles.container} >
-  //     <View style={styles.yellowView}/>
-  //     <View style={styles.buleView} />
-  //   </View>
-  // );
+export default class extends React.Component  {
+
+  getLocation = async() => {
+    const location = await Location.getCurrentPositionAsync()
+    console.log(location)
+  };
+
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    return <Loading />;
+  }
+
 }
 
 // const styles = StyleSheet.create({
